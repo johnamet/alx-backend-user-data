111404d74837d1f3dcc5a42d2565bf5e46473ba4
johnny <johnametepeagboku@gmail.com>
2024-07-14 00:39:31 +0000

    Update: session_auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -24,8 +24,20 @@ class SessionAuth(Auth):
         if not user_id or not isinstance(user_id, str):
             return None
 
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
 
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str) -> str:
+        """
+        Get the user id of the session
+        :param session_id: session id
+        :return: user id
+        """
+
+        if not session_id or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)

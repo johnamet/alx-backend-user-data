57f10f8f20f03a3e7b5b8150cf66619f9634b684
johnny <johnametepeagboku@gmail.com>
2024-07-14 01:08:47 +0000

    Update: session_auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@ The session auth module
 import uuid
 
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -41,3 +42,21 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+        get current user
+        """
+        session_id = self.session_cookie(request)
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        if not user_id:
+            return None
+
+        user = User.get(user_id)
+
+        if not user:
+            return None
+
+        return User.get(user_id)

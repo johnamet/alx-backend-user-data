bccddd89b3007b2fd706e1e9adf62a54dca824be
johnny <johnametepeagboku@gmail.com>
2024-07-14 00:31:46 +0000

    Update: session_auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """
 The session auth module
 """
+import uuid
 
 from api.v1.auth.auth import Auth
 
@@ -10,4 +11,21 @@ class SessionAuth(Auth):
     """
     Session Auth
     """
-    pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str) -> str:
+        """
+        Create a session
+        :param user_id: user id is the id of the user
+        :return: session id
+        """
+
+        if not user_id or not isinstance(user_id, str):
+            return None
+
+        session_id = uuid.uuid4()
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id

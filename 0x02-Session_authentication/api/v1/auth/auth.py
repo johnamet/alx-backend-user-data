b32407b328ceb90c13dcfc82987caadbf8689319
johnny <johnametepeagboku@gmail.com>
2024-07-14 00:13:15 +0000

    Initial Commit

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+The Authentication Module
+"""
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    The Basic Auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Defines path that requires auth
+        """
+
+        if not excluded_paths or not path:
+            return True
+
+        path = path.rstrip('/')
+
+        normalised_excluded_paths = {p.rstrip('/') for p in excluded_paths}
+
+        for excluded_path in normalised_excluded_paths:
+            if excluded_path.endswith("*"):
+                path_name = path.split('/')[-1]
+                excluded_path_name = excluded_path.split('/')[-1][:-1]
+                if path_name.startswith(excluded_path_name):
+                    return False
+
+        return not (path in normalised_excluded_paths)
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Flask request object
+        """
+        if request is None or request.headers.get('Authorization') is None:
+            return None
+
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Flask request object for current user
+        """
+
+        return None

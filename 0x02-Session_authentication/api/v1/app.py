2df3c9530c1964dd9bdc44a87dc7a5b0cec88a68
johnny <johnametepeagboku@gmail.com>
2024-07-14 00:21:30 +0000

    Initial COmmit: Session Auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,6 +6,7 @@ from os import getenv
 
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -18,8 +19,10 @@ auth = None
 
 if os.getenv("AUTH_TYPE") == "auth":
     auth = Auth()
-else:
+elif os.getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
+else:
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+The session auth module
+"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Session Auth
+    """
+    pass
